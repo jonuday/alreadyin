47c0a594aaf0ba61ffec542ac949bdbecf5e8cbb
Jonathan Day <jon@jonuday.com>
2018-05-08 05:50:43 -0700

    add cookie session methods to app

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 
 require('./models/User');
@@ -14,6 +16,18 @@ mongoose.connect(keys.mongoURI);
 
 // Declare the router.
 const app = express();
+
+// Use session cookies.
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000, // 30days
+		keys: [keys.cookieKey]
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Declare routes.
 require('./routes/authRoutes.js')(app);
 
 // Start server on Heroku or port 5000 locally.
